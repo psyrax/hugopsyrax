d4e983de79343b2ec816afafa189dbdf96e6703e
Psyrax <psyrax@opiumgarden.org>
2016-01-20 09:50:30 -0600

    prax pallooza

---
+++ content/pallooza/thnx.md
@@ -0,0 +1,12 @@
++++
+date = "2016-01-20T09:48:22-06:00"
+draft = false
+title = "Gracias por tu pago"
+
++++
+
+<div class="row">
+	<div class="col-xs-12">
+		<img src="/images/deadpool.jpg">
+	</div>
+</div>
--- themes/psyrax012Theme/static/js/app.js
+++ themes/psyrax012Theme/static/js/app.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
 	particlesJS.load('particles-js','/js/particlesConfig.json',function(){});
+	$('#ticketAmount').on('change', function	() {
+		var totalTicket = parseInt($(this).val()) * 65;
+		$('#amountPP').val(totalTicket);
+		$('#ticketTotal').html('$'+totalTicket);
+	})
 });
