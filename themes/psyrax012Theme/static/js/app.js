3fa3f63163390183bef8b60d97f49392240fb9f3
Jose Luis Espinoza <psyrax@Joses-MacBook-Pro.local>
2016-02-19 12:32:57 -0600

    palloza upgrade push

--- themes/psyrax012Theme/static/js/app.js
+++ themes/psyrax012Theme/static/js/app.js
@@ -1,8 +1,13 @@
 $(document).ready(function(){
 	particlesJS.load('particles-js','/js/particlesConfig.json',function(){});
 	$('#ticketAmount').on('change', function	() {
-		var totalTicket = parseInt($(this).val()) * 65;
+		var totalTicket = parseInt($(this).val()) * 96;
 		$('#amountPP').val(totalTicket);
 		$('#ticketTotal').html('$'+totalTicket);
+	});
+	$('#ticketAmountUpgrade').on('change', function	() {
+		var totalTicket = parseInt($(this).val()) * 30;
+		$('#amountPPUpgrade').val(totalTicket);
+		$('#ticketTotalUpgrade').html('$'+totalTicket);
 	})
 });
